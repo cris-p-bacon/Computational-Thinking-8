7f406613719916e3fa5c0481751e5095b535ba2c
cris-p-bacon <ericcisyk@seattleacademy.org>
2025-12-15 18:44:13 +0000

    Project 2

---
+++ Projects/adventure.py
@@ -0,0 +1,14 @@
+answer1 = ("School is over - where do you want to go?")
+if answer1 == "Basketball Game":
+
+        answer2 = input("Do you want to get menchies")
+        if answer2 == "Yes":
+                print("Youget some popcorn.")
+        if answer2 == "No" :
+                print("You find a seat with your friend.")      
+
+if answer1 == "leave school" :
+
+    answer3 = input("Do you want to go directly home?")
+    if answer3 == "Yes" :
+           print
---
+++ Projects/project2.py
@@ -0,0 +1,67 @@
+print("Welcome to the Personality Quiz!")
+print("Select A, B, or C for each question, depending on your preferences.\n")
+
+# Declare variables
+extrovert_points = 0
+introvert_points = 0
+omnivert_points = 0
+
+# Question 1
+answer1 = input("Do you prefer A going to parties frequently, B rarely going to parties, or C it depends.\n")
+if answer1 == "A":
+    extrovert_points += 1
+elif answer1 == "B":
+    introvert_points += 1
+elif answer1 == "C":
+    omnivert_points += 1
+print("------------------------------------------------------------------------------------------------------------------------------------------------------- ")
+
+# Question 2
+answer2 = input("Do you prefer A hanging out with a lot of different people, B hanging out with a core group of people, or C both.\n")
+if answer2 == "A":
+    extrovert_points += 1
+elif answer2 == "B":
+    introvert_points += 1
+elif answer2 == "C":
+    omnivert_points += 1
+print("------------------------------------------------------------------------------------------------------------------------------------------------------- ")
+
+# Question 3
+answer3 = input("Do you A feel energized after large social gathering, B Feel drained after hanging out with a lot of people, or C both.\n")
+if answer3 == "A":
+    extrovert_points += 1
+elif answer3 == "B":
+    introvert_points += 1
+elif answer3 == "C":
+    omnivert_points += 1
+print("------------------------------------------------------------------------------------------------------------------------------------------------------- ")
+
+# Question 4
+answer4 = input("Do you A like to work on projects with a group, B work alone, or C it depends?\n")
+if answer4 == "A" or answer4 == "C":
+    extrovert_points += 1
+    omnivert_points += 1
+elif answer4 == "B":
+    introvert_points += 1
+print("------------------------------------------------------------------------------------------------------------------------------------------------------- ")
+answer5 = input("Do you A enjoy small talk, B find it tiresome, or C you are indifferent.\n")
+
+# Question 5
+if answer5 == "A":
+    extrovert_points += 1
+elif answer4 == "B":   
+    introvert_points += 1
+elif answer5 == "C":
+    omnivert_points += 1
+print("------------------------------------------------------------------------------------------------------------------------------------------------------- ")
+
+# Print the score
+print (f"Your score is {extrovert_points} extrovert, {introvert_points} introvert, and {omnivert_points} omnivert.")
+
+# End: determine result
+if extrovert_points >= introvert_points and extrovert_points >= omnivert_points:
+    print("You are an Extrovert!!!")
+elif introvert_points >= omnivert_points and introvert_points >= extrovert_points:
+    print("You are an Introvert!!!")
+elif omnivert_points >= extrovert_points and omnivert_points >= introvert_points:
+    print("You are an Omnivert!!!")
